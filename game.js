7d77638d4f93e9e8431d81943fb28edccba3f9d8
Andromeda1223 <109067452+Andromeda1223@users.noreply.github.com>
2022-07-27 16:51:26 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -15,7 +15,8 @@ const Game = {
     image: new Image(),
     src: "imagenes/portada 3 con titulo mono.jpg",
     policias: [],
-    
+    arrayVidas: [],
+
     init() {
         this.canvas = document.getElementById("myCanvas")
         this.ctx = this.canvas.getContext("2d")
@@ -38,6 +39,7 @@ const Game = {
 
             this.clear()
             this.drawAll()
+            this.generateVidas()
             this.generatePersonas()
             this.generatePolicias()
             this.isCollision()
@@ -45,8 +47,10 @@ const Game = {
             this.contadorDePuntuacion(this.puntuacion)
             this.contadorDeVidas(this.vidas)
             this.cambiadorDeSkin()
-            
-            
+            this.isCollisionPolichia()
+            this.colisionVidas()
+
+
 
         }, 1000 / this.FPS)
     },
@@ -55,8 +59,8 @@ const Game = {
     createAll() {
         this.fondo = new Fondo(this.ctx, this.width, this.height)
         this.player = new Player(this.ctx, this.width, this.height)
-        
-    
+
+
     },
 
     drawAll() {
@@ -69,7 +73,10 @@ const Game = {
         this.policias.forEach((policias) => {
             policias.draw()
         })
-        
+       
+        this.arrayVidas.forEach((policias) => {
+            policias.draw()
+        })
     }
 
     ,
@@ -78,12 +85,12 @@ const Game = {
 
     },
     generatePersonas() {
-        if (this.framesCounter % 200 === 0) {
+        if (this.framesCounter % 300 === 0) {
             this.personas.push(new Persona(this.ctx, this.width, this.player.posY0, this.player.height, "imagenes/victima_2-removebg-preview.png"))
-            
+
 
         }
-        if (this.framesCounter % 120 === 0){
+        if (this.framesCounter % 120 === 0) {
             this.personas.push(new Persona(this.ctx, this.width, this.player.posY0, this.player.height, "imagenes/victima_1-preview-removebg-preview.png"))
 
         }
@@ -96,8 +103,7 @@ const Game = {
                 this.player.posY < personas.posY + personas.height &&
                 this.player.height + this.player.posY > personas.posY) {
                 this.personas.splice(index, 1)
-                this.vidas--
-                
+
                 this.puntuacion = this.puntuacion - 10
 
             }
@@ -109,63 +115,111 @@ const Game = {
             this.player.gameover.play()
         }
     },
-    colisionesMachete(){
-        this.player.bullets.forEach((bullets, index1)=>{
-          this.personas.forEach((obstaclee, index2)=>{
-            if (bullets.posX < obstaclee.posX + obstaclee.width &&
-              bullets.posX > obstaclee.posX &&
-              bullets.posY < obstaclee.posY + obstaclee.height &&
-            bullets.posY > obstaclee.posY){
-                this.sonido()
-                this.personas.splice(index2, 1)
-                this.player.bullets.splice(index1, 1)
-                this.puntuacion = this.puntuacion + 10
-                
+    colisionesMachete() {
+        this.player.bullets.forEach((bullets, index1) => {
+            this.personas.forEach((obstaclee, index2) => {
+                if (bullets.posX < obstaclee.posX + obstaclee.width &&
+                    bullets.posX > obstaclee.posX &&
+                    bullets.posY < obstaclee.posY + obstaclee.height &&
+                    bullets.posY > obstaclee.posY) {
+                    this.sonido()
+                    this.personas.splice(index2, 1)
+                    this.player.bullets.splice(index1, 1)
+                    this.puntuacion = this.puntuacion + 10
 
 
-                
 
-                
-              }
-          })
+
+
+
+                }
+            })
         })
-},
-sonido(){
-    this.chucilloMusica.play()
+    },
+    sonido() {
+        this.chucilloMusica.play()
 
-},
-contadorDePuntuacion(puntuacion){
-    this.ctx.fillStyle = "white"
-    this.ctx.font = '30px arial'
-    this.ctx.fillText(`Score: ${puntuacion}`, 50, 50)
-  },
-contadorDeVidas(vidas){
-    this.ctx.fillStyle = "white"
-    this.ctx.font = '30px arial'
-    this.ctx.fillText(`Vidas: ${vidas}`, 50, 100)
-},
+    },
+    contadorDePuntuacion(puntuacion) {
+        this.ctx.fillStyle = "white"
+        this.ctx.font = '30px arial'
+        this.ctx.fillText(`Score: ${puntuacion}`, 50, 50)
+    },
+    contadorDeVidas(vidas) {
+        this.ctx.fillStyle = "white"
+        this.ctx.font = '30px arial'
+        this.ctx.fillText(`Vidas: ${vidas}`, 50, 100)
+    },
 
-cambiadorDeSkin(){
-    if (this.puntuacion === 200){
-        this.player.cambiarSkin()
-        this.puntuacion = this.puntuacion + 10
-}
-if (this.puntuacion === 400){
-    this.player.cambiarSkin()
-    this.puntuacion = this.puntuacion + 10
-}
-if (this.puntuacion === 600){
-    this.player.cambiarSkin()
-    this.puntuacion = this.puntuacion + 10
-}
-},
-generatePolicias() {
-    if (this.framesCounter % 210 === 0) {
-        this.policias.push(new Policia(this.ctx, this.width, this.player.posY0, this.player.height, "imagenes/policia__1_-removebg-preview.png"))
+    cambiadorDeSkin() {
+        if (this.puntuacion === 200) {
+            this.player.cambiarSkin()
+            this.puntuacion = this.puntuacion + 10
+        }
+        if (this.puntuacion === 400) {
+            this.player.cambiarSkin()
+            this.puntuacion = this.puntuacion + 10
+        }
+        if (this.puntuacion === 600) {
+            this.player.cambiarSkin()
+            this.puntuacion = this.puntuacion + 10
+        }
+        if (this.puntuacion === 800) {
+            this.player.cambiarSkin()
+            this.puntuacion = this.puntuacion + 10
+        }
+        if (this.puntuacion === 100) {
+            this.player.cambiarSkin()
+            this.puntuacion = this.puntuacion + 10
+        }
+        if (this.puntuacion === 1200) {
+            this.player.cambiarSkin()
+            this.puntuacion = this.puntuacion + 10
+        }
+    },
+    generateVidas() {
+        if (this.framesCounter % 1000 === 0) {
+            this.arrayVidas.push(new Vida(this.ctx, this.width, this.player.posY0, this.player.height, "imagenes/2767b988ec94acbd05327e733baff3cc-removebg-preview.png"))
+        }
+    },
+    generatePolicias() {
+        if (this.framesCounter % 450 === 0) {
+            this.policias.push(new Policia(this.ctx, this.width, this.player.posY0, this.player.height, "imagenes/policia__1_-removebg-preview.png"))
+
+
+        }
+        if (this.framesCounter % 523 === 0) {
+            this.policias.push(new Policia(this.ctx, this.width, this.player.posY0, this.player.height, "imagenes/SaraConor.png"))
+        }
+
+    },
+    isCollisionPolichia() {
+        this.policias.forEach((personas, index) => {
+            if (this.player.posX < personas.posX + personas.width &&
+                this.player.posX + this.player.width > personas.posX &&
+                this.player.posY < personas.posY + personas.height &&
+                this.player.height + this.player.posY > personas.posY) {
+
+                this.policias.splice(index, 1)
+                this.vidas--
+            }
+
+        })
+
+    },
+    musicaFondo() {
         
+    },
+colisionVidas(){
+        this.arrayVidas.forEach((personas, index) => {
+            if (this.player.posX < personas.posX + personas.width &&
+                this.player.posX + this.player.width > personas.posX &&
+                this.player.posY < personas.posY + personas.height &&
+                this.player.height + this.player.posY > personas.posY) {
+                    this.arrayVidas.splice(index, 1)
+                this.vidas ++
+        }
+    })
 
-    }
-    
 },
 }
-
